228db9a2d00bb3b5f1fb742b632f59982e5a6de0
houssemELbahri <houssembahri1995@gmail.com>
2022-05-24 14:38:35 +0100

    connect to DB

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
+const port = 3000;
 const app = express();
+console.log(process.env.MONGO_URI);
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log("server is running on port 3000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
